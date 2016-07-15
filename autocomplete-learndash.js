59a3765d974bbdcf62aef437cac9b815fc6b87c4
Peter Malcolm <peter.t.malcolm@gmail.com>
2016-07-15 14:02:18 -0400

    autocomplete is now IIFE

--- autocomplete-learndash.js
+++ autocomplete-learndash.js
@@ -1,3 +1,5 @@
 // Scitent bookmarklet code to autocomplete LearnDash courses
-jQuery('[value="1"]').attr('checked','checked');
-jQuery('[value="Next"]').trigger('click');
+(function(){ /* code */ 
+	jQuery('[value="1"]').attr('checked','checked');
+	jQuery('[value="Next"]').trigger('click');
+}());
