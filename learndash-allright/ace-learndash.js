85d31d3f1e50d89a4a49c613af94503a7568d116
Peter Malcolm <peter.t.malcolm@gmail.com>
2016-08-01 11:31:34 -0400

    skeleton for acing ld quizzes

---
+++ learndash-allright/ace-learndash.js
@@ -0,0 +1,5 @@
+// Scitent bookmarklet code to autocomplete LearnDash courses with all correct answers
+(function(){ /* code */ 
+	jQuery('[value="1"]').attr('checked','checked');
+	jQuery('[value="Next"]').trigger('click');
+}());
