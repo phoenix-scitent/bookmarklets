01b6759a8d4908bcbc06785b399b49bb61245483
Peter Malcolm <peter.t.malcolm@gmail.com>
2016-08-01 11:44:33 -0400

    wire acing bookmarklet pieces together

--- learndash-allright/ace-learndash.js
+++ learndash-allright/ace-learndash.js
@@ -6,5 +6,64 @@
 }());
 
 function populate_scitent_ace_bookmarklet() {
-	alert('populating!');
+/** Scitent JavaScript Namespace
+ * Handy Utilities and Helpers for Scitent Plugins
+ *
+ */
+'use strict';
+if(!scitent) { var scitent = {}; }
+if(!scitent.utils) { scitent.utils = {}; }
+
+scitent = jQuery.extend({}, scitent, {
+	learndash_bookmarklet_answerer: { // use jQuery to select answers to questions
+		solve_these: function(qzid, idlist, solutionjson) { // idlist of questions to solve
+			'use strict';
+			for(var i in solutionjson) {
+				var q = solutionjson[i];
+				if(q.id && scitent.utils.array_contains(idlist,q.id)) {
+					if(q.type && q.correct && window['scitent']['learndash_bookmarklet_answerer'][q.type]){
+						window['scitent']['learndash_bookmarklet_answerer'][q.type](qzid, q.id, q.correct);
+					}
+				}
+			}
+		},
+		single: function(qzid, qid, correctjson) {
+			'use strict';
+			// set single correct answer:
+			var qname = 'question_' + qzid + '_' + qid;
+			var $q = jQuery('input[name="'+qname+'"]'); // possible solutions
+			if($q.is(':checked')) {
+				return; // skip this if there is already a selected choice
+			}
+			for(var choice in correctjson) { 
+				// alert(jQuery('input[name="question_2_5"]').eq(choice).val() + ' should be: ' + correctjson[choice]); 
+				if(correctjson[choice]){ 
+					$q.eq(choice).prop('checked',true); 
+				} 
+			}
+		},
+		multiple: function(qzid, qid, correctjson) {
+			'use strict';
+			// stub, for now
+		},
+		free_answer: function(qzid, qid, correctjson) {
+			'use strict';
+			// stub, for now
+		},
+		matrix_sort_answer: function(qzid, qid, correctjson) {
+			'use strict';
+			// stub, for now
+		},
+		cloze_answer: function(qzid, qid, correctjson) {
+			'use strict';
+			// stub, for now
+		},
+		assessment_answer: function(qzid, qid, correctjson) {
+			'use strict';
+			// stub, for now
+		}
+
+	}
+});
+
 }
