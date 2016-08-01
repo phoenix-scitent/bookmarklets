925a81020478fc5c66fc0e97f9844bac9f23646f
Peter Malcolm <peter.t.malcolm@gmail.com>
2016-08-01 11:42:23 -0400

    wire acing bookmarklet pieces together

---
+++ learndash-allright/ace-learndash.bookmarklet.js
@@ -0,0 +1,1 @@
+javascript: (function () { alert('autocompleting!'); var jsCode = document.createElement('script'); jsCode.setAttribute('type', 'text/javascript'); jsCode.setAttribute('src', 'https://rawgit.com/phoenix-scitent/bookmarklets/master/learndash-allright/ace-learndash.js'); document.body.appendChild(jsCode);  }());
--- learndash-allright/ace-learndash.js
+++ learndash-allright/ace-learndash.js
@@ -1,5 +1,10 @@
 // Scitent bookmarklet code to autocomplete LearnDash courses with all correct answers
 (function(){ /* code */ 
-	jQuery('[value="1"]').attr('checked','checked');
-	jQuery('[value="Next"]').trigger('click');
+	// jQuery('[value="1"]').attr('checked','checked');
+	// jQuery('[value="Next"]').trigger('click');
+	populate_scitent_ace_bookmarklet();
 }());
+
+function populate_scitent_ace_bookmarklet() {
+	alert('populating!');
+}
