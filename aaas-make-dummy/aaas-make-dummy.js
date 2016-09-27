b25865d2bd5226d58a578dbeb1df0b081118333f
Peter Malcolm <peter.t.malcolm@gmail.com>
2016-09-27 11:40:19 -0400

    expand bookmarklet for billing form too

--- aaas-make-dummy/aaas-make-dummy.js
+++ aaas-make-dummy/aaas-make-dummy.js
@@ -2,7 +2,11 @@
   var suffix = '_' + (parseInt(Math.random()*10000000)).toString(16);
   var formdata =
   {'first_name-':'Dude',
+   'billing_first_name':'Dude',
    'last_name-':'Ranch',
+   'billing_last_name':'Ranch',
+   'billing_address':'123 Somewhere',
+   'billing_city':'Somewhereville',
    'user_pass-':'Password',
    'user_pass_confirm-':'Password',
    'institution':'Dudes United'
@@ -11,4 +15,6 @@
     $('[id^="'+field+'"').val(formdata[field] + suffix );
   }
   $('[id^="user_email"').val( suffix + '@dispostable.com' );
+  $('[id^="billing_email"').val( suffix + '@dispostable.com' );
+  $('[id^="billing_postcode"').val( '12345' );
 }(jQuery));
